c9d5c5081e8b7a162ae5a7d0c3ab8b3c8c263fd9
Tahir Ismail <Tahir.pe@gmail.com>
2026-08-24 19:57:25 +0200

    fix: add missing model and service files from earlier commits

---
+++ CoffeeNChill.Functions/Models/StaffDocumentInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace CoffeeNChill.Functions.Models;
+
+public class StaffDocumentInfo
+{
+    public string FileName { get; set; } = string.Empty;
+    public long SizeInBytes { get; set; }
+    public DateTimeOffset LastModified { get; set; }
+}
