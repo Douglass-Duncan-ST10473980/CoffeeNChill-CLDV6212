e0025291b8f5a3dbbf7340ae2e7e7452fc56f32a
Tahir Ismail <Tahir.pe@gmail.com>
2026-09-07 19:00:16 +0200

    docs: add author attribution to Document API classes

--- CoffeeNChill.Functions/Functions/DocumentFunctions.cs
+++ CoffeeNChill.Functions/Functions/DocumentFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿//Author: Tahir Ismail
+using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
