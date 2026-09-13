07496b4201ba34a3d3886aced4c41d3bd7a21803
Douglass-Duncan-ST10473980 <st10473980@myemeris.edu.za>
2026-09-13 11:29:32 +0200

    Removed the request DTO from file

--- CoffeeNChill.Functions/Models/MenuItem.cs
+++ CoffeeNChill.Functions/Models/MenuItem.cs
@@ -47,15 +47,5 @@ namespace CoffeeNChill.Functions.Models
         public double Price { get; set; }
         public bool IsAvailable { get; set; }
     }
-
-    // Data Transfer Object (DTO) for Incoming HTTP Requests
-    public class MenuItemRequest
-    {
-        public string Category { get; set; } = string.Empty;
-        public string SKU { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public double Price { get; set; }
-        public bool IsAvailable { get; set; } = true;
-    }
+    
 }
